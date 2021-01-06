6df7f276f2b5903c53bf52b99d863fc76d58bf4e
athenianowl2100 <nathanlintest@gmail.com>
2021-01-06 18:16:25 +0000

    First draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,12 +2,34 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int[] shuffled = new int[arr.length];
+
+        int k = 0;
+
+        for (int j = 0; j < arr.length/2; j++) {
+            shuffled[k] = arr[j];
+            k+=2;
+        }
+
+        k = 1;
+
+        for (int j = arr.length/2; j < arr.length; j++) {
+            shuffled[k] = arr[j];
+            k+=2;
+        }
+
+        return shuffled;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            int swap = (int) (arr.length * Math.random());
+            int x = arr[i];
+            arr[i] = arr[swap];
+            arr[swap] = x;
+        }
+        return arr;
     }
 
 
